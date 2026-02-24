df375a60d741540b0573c06827f7d1f4180a56a6
Ruben Tri Ardian Saputra <rubentriardians@gmail.com>
2026-02-24 23:28:28 +0800

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
+use App\Http\Controllers\AdminUserController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+// Route untuk dashboard (default Breeze)
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route untuk Admin - SATU GROUP AJA
+Route::middleware(['auth', 'role:Admin'])->prefix('admin')->name('admin.')->group(function () {
+  Route::get('/dashboard', function () {
+    return view('admin');
+  })->name('dashboard');
+
+  // Route untuk user management
+  Route::resource('users', AdminUserController::class);
+});
+
+// Route untuk Kasir
+Route::middleware(['auth', 'role:Kasir'])->group(function () {
+  Route::get('/cashier', function () {
+    return view('cashier');
+  })->name('cashier.dashboard');
 });
